7ee2ceeeba3bd2231d510122ec531767c28ef1b5
SewarAslan <s12028585@stu.najah.edu>
2024-11-22 19:14:57 +0200

    adding id to bouquetView model to handle when click2

--- backend/model/item.model.js
+++ backend/model/item.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const db = require('../config/db');
 
 const itemSchema = new mongoose.Schema({
+  id: {type: String, required: true },
   name: { type: String, required: true },
   flowerType: { type: String, required: true },
   tags: { type: [String], required: true },
