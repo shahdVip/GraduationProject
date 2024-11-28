dde3f0a8d2dc84ad11aa12fa4e14492c4255f6d6
SewarAslan <s12028585@stu.najah.edu>
2024-11-28 14:46:27 +0200

    image processing-color detect feature

---
+++ grad_roze/lib/pages/cameraPage/camera_page_model.dart
@@ -0,0 +1,16 @@
+import '/custom/theme.dart';
+import '/custom/util.dart';
+import '/custom/widgets.dart';
+import 'camera_page_widget.dart' show CameraPageWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class CameraPageModel extends FlutterFlowModel<CameraPageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
