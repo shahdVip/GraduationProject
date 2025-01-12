ecff4e9f80e07ba84352d7083b9d404b0c7e522e
shahdVip <shsalahat2002@gmail.com>
2025-01-12 02:19:38 +0200

    qq

--- grad_roze/lib/pages/chat_page/chat_page.dart
+++ grad_roze/lib/pages/chat_page/chat_page.dart
@@ -1,7 +1,15 @@
+import 'dart:convert';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:grad_roze/config.dart';
 import 'package:grad_roze/custom/theme.dart';
 import 'package:grad_roze/custom/util.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChatPage extends StatefulWidget {
   final String chatId; // e.g., customerId_businessId
@@ -23,11 +31,110 @@ class _ChatPageState extends State<ChatPage> {
   final TextEditingController _messageController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
   String? receiverId; // To store the receiver's ID
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  String? deviceToken;
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin(); // Initialize the plugin
 
   @override
   void initState() {
     super.initState();
+    _initializeFirebaseMessaging();
+    _initializeNotificationChannel();
     _fetchReceiverDetails();
+  }
+
+  Future<void> sendDeviceTokenToBackend(String token) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? username = prefs.getString('username'); // Replace with your key
+
+      final response = await http.post(
+        Uri.parse('$url/save-device-token'),
+        headers: {
+          'Content-Type':
+              'application/json', // Set the content type to application/json
+        },
+        body: json.encode({
+          'deviceToken': token,
+          'username':
+              username, // Replace with actual username or user identifier
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Device token sent successfully');
+      } else {
+        print('Failed to send device token ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error sending device token: $e');
+    }
+  }
+
+  Future<void> _initializeFirebaseMessaging() async {
+    // Get the device token
+    deviceToken = await messaging.getToken();
+    print("Device Token: $deviceToken");
+    sendDeviceTokenToBackend(deviceToken!);
+    // Listen to foreground notifications
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Foreground Message: ${message.notification?.title}');
+      // Handle the notification
+      _showNotification(message);
+    });
+
+    // Handle background notifications
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Background Message: ${message.notification?.title}');
+      // Navigate to a specific screen if needed
+    });
+  }
+
+// Function to initialize notification channel
+  Future<void> _initializeNotificationChannel() async {
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'message_notifications', // ID must match with AndroidNotificationDetails
+      'Message Notifications', // Channel name
+      description: 'Your channel description',
+      importance: Importance.high,
+    );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+  }
+
+  Future<void> _showNotification(RemoteMessage message) async {
+    try {
+      print("Attempting to show notification...");
+      var androidDetails = AndroidNotificationDetails(
+        'message_notifications',
+        'Message Notifications',
+        channelDescription: 'Your channel description',
+        importance: Importance.high,
+        priority: Priority.high,
+      );
+
+      var generalNotificationDetails = NotificationDetails(
+        android: androidDetails,
+      );
+
+      await flutterLocalNotificationsPlugin.show(
+        0,
+        message.notification?.title ?? "No Title",
+        message.notification?.body ?? "No Body",
+        generalNotificationDetails,
+      );
+      print("Notification displayed successfully.");
+    } catch (e) {
+      print("Error displaying notification: $e");
+    }
   }
 
   // Function to fetch the receiver's details
@@ -50,8 +157,7 @@ class _ChatPageState extends State<ChatPage> {
     }
   }
 
-  // Function to send a message
-  void sendMessage() async {
+  Future<void> sendMessage() async {
     String message = _messageController.text.trim();
     if (message.isNotEmpty) {
       // Check if the chat already exists, if not create a new chat
@@ -92,6 +198,43 @@ class _ChatPageState extends State<ChatPage> {
         duration: Duration(milliseconds: 300),
         curve: Curves.easeOut,
       );
+
+      // Fetch the receiver's FCM token
+      try {
+        String? receiverToken =
+            "c00yJFUcQACgGMkE-vMtYc:APA91bFW2I4qaoz68VsdzbPPyu7t-SKqMEbK3zXrirI-JyZl7ZZ2B9uox4NqaHndIjfjopvFIdbzZ7g_Fqu9v9_3ogzEYH-qVB2HBMwITPLE8_pEFxTxhj0"; // Assuming you store the FCM token in 'fcmToken'
+        // Send a push notification to the receiver via your backend
+        await sendPushNotificationToBackend(
+            receiverToken, 'New Message from ${widget.userId}', message);
+      } catch (e) {
+        print("Error fetching receiver's FCM token: $e");
+      }
+    }
+  }
+
+// Function to send push notification to backend
+  Future<void> sendPushNotificationToBackend(
+      String deviceToken, String title, String message) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$url/notification/send-message'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'deviceToken': deviceToken,
+          'title': title,
+          'message': message,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Push notification sent successfully');
+      } else {
+        print('Failed to send push notification: ${response.body}');
+      }
+    } catch (e) {
+      print('Error sending push notification: $e');
     }
   }
 
